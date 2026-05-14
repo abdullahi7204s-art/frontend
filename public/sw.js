ff49b0ceffa18288adf454d3c690a769d58dba90
abdullahi7204s-art <abdullahi7204s@gmail.com>
2026-05-14 15:21:10 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,14 @@
+// This is the Service Worker (sw.js)
 self.addEventListener('install', (e) => {
   console.log('SpendWise Service Worker Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
 });
 
 self.addEventListener('fetch', (e) => {
+  // Keeps the app running smoothly
   e.respondWith(fetch(e.request));
 });
