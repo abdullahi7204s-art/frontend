207528b227f4d049762205c77f64432d398a92c5
abdullahi7204s-art <abdullahi7204s@gmail.com>
2026-05-14 14:55:24 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service worker registered!'))
+      .catch(err => console.log('Service worker failed:', err));
+  });
+}
